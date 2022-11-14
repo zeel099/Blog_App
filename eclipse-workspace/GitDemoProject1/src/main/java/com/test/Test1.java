bdeea5d7a08bca94b7a9423f208cab69515e9c5e
zeelsojitra <zeelsojitra@192.168.1.8>
2022-11-14 20:25:35 +0530

    first commit

---
+++ eclipse-workspace/GitDemoProject1/src/main/java/com/test/Test1.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Test1 {
+
+	public static void main(String args[]) {
+		 String str = "ndkn";
+				
+				if(str == "ndkn") {
+					System.out.println("true");
+				}
+				else {
+					System.out.println("false");
+				}
+	}
+	
+}
